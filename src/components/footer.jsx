7200c2e058372446fe1a78ff827845554fa06ae3
Junaid <33651074+jaslam94@users.noreply.github.com>
2021-12-13 23:45:16 +0500

    added my profile link to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,8 +3,20 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="footer mt-auto py-3 bg-dark">
-      <div className="container">
+      <div className="container d-flex flex-column">
         <span className="text-white-50">Air Mate © 2021</span>
+        <span className="text-white-50">
+          Designed & Developed by
+          <strong>
+            <a
+              href="https://www.linkedin.com/in/mjunaidaslam/"
+              className="text-decoration-none text-white-50"
+            >
+              {" "}
+              Junaid Aslam
+            </a>
+          </strong>
+        </span>
       </div>
     </footer>
   );
