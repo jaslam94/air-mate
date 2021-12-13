72b1d355642cb35bed073ab48a404d29441aaff5
Junaid <33651074+jaslam94@users.noreply.github.com>
2021-12-13 23:50:24 +0500

    style profile link in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,18 +5,16 @@ export default function Footer() {
     <footer className="footer mt-auto py-3 bg-dark">
       <div className="container d-flex flex-column">
         <span className="text-white-50">Air Mate © 2021</span>
-        <span className="text-white-50">
+        <small className="text-muted">
           Designed & Developed by
-          <strong>
-            <a
-              href="https://www.linkedin.com/in/mjunaidaslam/"
-              className="text-decoration-none text-white-50"
-            >
-              {" "}
-              Junaid Aslam
-            </a>
-          </strong>
-        </span>
+          <a
+            href="https://www.linkedin.com/in/mjunaidaslam/"
+            className="text-decoration-none text-white-50"
+          >
+            {" "}
+            Junaid Aslam
+          </a>
+        </small>
       </div>
     </footer>
   );
